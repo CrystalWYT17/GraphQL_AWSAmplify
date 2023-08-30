d2db66a81a415e47aa75f83ad5a6d021195ec824
CrystalWYT17 <wuttyeetun1721999@gmail.com>
2023-08-30 23:17:35 +0800

    add console output

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(
   })
 );
 
+console.log("user ", process.env.mongoUsername);
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.mongoUsername}:${process.env.mongoUserPassword}@graphqlcluster.vhhygv4.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`,
